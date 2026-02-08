74cf47da62c02b5b90f8da764fe6f4816aa59077
vinnybrunn00 <vinibruno.dev@gmail.com>
2026-02-08 12:56:24 -0300

    stream message + get resume the chat with gemini

---
+++ lib/services/test/test.ts
@@ -0,0 +1,18 @@
+import { Message, Client } from "@open-wa/wa-automate";
+import { MessageServices } from "../../interfaces/message-services";
+import fs from "fs/promises";
+import GeminiFunctions from "../IA/gemini-functions";
+
+export default class Test extends GeminiFunctions implements MessageServices {
+    private command: string = "____!resume";
+    private root: string = process.cwd();
+
+    public validateCommand(message: Message): boolean {
+        if (!message.isGroupMsg) return false;
+        return message.body.startsWith(this.command);
+    }
+
+    public async handle(message: Message, bot: Client): Promise<void> {}
+
+    private async test(message: Message, bot: Client): Promise<void> {}
+}
